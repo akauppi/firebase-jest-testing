af7dced79daabee2f09efa47b0667e961e91b4b7
Asko Kauppi <akauppi@gmail.com>
2021-06-25 18:31:53 +0300

    fixing CI (using newer image); changes in the fns userInfo test (unrelated)

--- sample/test-fns/userInfo.test.js
+++ sample/test-fns/userInfo.test.js
@@ -18,9 +18,6 @@ describe("userInfo shadowing", () => {
     preheat_EXP("projects/1/userInfo");
   })
 
-  // Note: We don't declare 'async done => ...' for Jest. That is an oxymoron: only either 'done' or the end of an
-  //    async/await body would resolve a test but not both.
-  //
   test('Central user information is distributed to a project where the user is a member', async () => {
     const william = {
       displayName: "William D.",
@@ -28,17 +25,18 @@ describe("userInfo shadowing", () => {
     };
 
     // Write in 'userInfo' -> causes a Cloud Function to update 'projectC/{project-id}/userInfo/{uid}'
+    //
     await collection("userInfo").doc("abc").set(william);
 
     // Style 1:
     //  - 'eventually' only passes when the right kind of object is there.
     //
-    await expect( eventually("projects/1/userInfo/abc", o => o && shallowEquals(o,william)) ).resolves.not.toThrow();
+    //await expect( eventually("projects/1/userInfo/abc", o => o && shallowEquals(o,william)) ).resolves.not.toThrow();
 
     // Style 2:
     //  - 'eventually' passes on first valid doc, checking is done outside of 'expect'.
     //
-    //await expect( eventually("projects/1/userInfo/abc") ).resolves.toContainObject(william);
+    await expect( eventually("projects/1/userInfo/abc") ).resolves.toContainObject(william);
   });
 
   test('Central user information is not distributed to a project where the user is not a member', async () => {
@@ -47,7 +45,10 @@ describe("userInfo shadowing", () => {
     //
     await collection("userInfo").doc("xyz").set({ displayName: "blah", photoURL: "https://no-such.png" });
 
-    await expect( eventually("projects/1/userInfo/xyz", o => !!o, 300 /*ms*/) ).resolves.toBeUndefined();
+    // Note: The timeout should be in the same magnitude - but slightly higher than - the known "pass" times of the above
+    //    test (with per-heating).
+    //
+    await expect( eventually("projects/1/userInfo/xyz", o => !!o, 500 /*ms*/) ).resolves.toBeUndefined();
 
     // Ideally:
     //await expect( eventually("projects/1/userInfo/xyz") ).resolves.toBeUndefined()
