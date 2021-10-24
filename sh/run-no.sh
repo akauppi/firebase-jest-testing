a6dd47064b6a621536059438c70f78e95d872ec3
Asko Kauppi <akauppi@gmail.com>
2021-10-24 15:36:31 +0300

    Dependency upgrades; mitigating a critical warning

---
+++ sh/run-no.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# List possibly outdated dependencies, in all the subpackages.
+#
+PATHS=". sample/functions package"
+
+for _PATH in $PATHS
+do
+  # '|| true': carry on even if there are listed entries
+  npm --prefix "$_PATH" outdated || true
+done
